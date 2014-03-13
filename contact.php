339b87c3bbe10e9b0ba0675a8b9b5d632dac0f65
earlieclark <earl@mockitbrd.com>
2014-03-13 08:13:26 -0400

    Final Commit To Kunle’s Website

---
+++ contact.php
@@ -0,0 +1,24 @@
+<?php
+
+// Gathering Information from The User input
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$msg = $_POST['message'];
+
+$myAdd = "vice2010@gmail.com";
+$message = 'From: '.$name.' Email: '.$email.' Message: '.$msg;
+$headers = 'From: '. $email . "\r\n";
+
+// Contains the email parameters
+
+$success = mail($myAdd, $message, $headers);
+
+// Sends the email if a success
+	if ($success) {
+		echo "true";
+	}  else {
+		echo "false";
+	}
+
+?>
