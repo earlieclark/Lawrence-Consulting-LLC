f6af6810d231d6a21bf82408bff504921b9c40f9
earlieclark <earl@mockitbrd.com>
2014-04-15 07:37:29 -0400

    Final Commit To Kunle Website

--- contact.php
+++ contact.php
@@ -1,18 +1,19 @@
 <?php
 
 // Gathering Information from The User input
-
 $name = $_POST['name'];
 $email = $_POST['email'];
-$msg = $_POST['message'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
-$myAdd = "info@lawrenceconsultingllc.com";
-$message = 'From: '.$name.' Email: '.$email.' Message: '.$msg;
-$headers = 'From: '. $email . "\r\n";
+	$to = 'info@lawrenceconsultingllc.com';
+	$subject = 'The Subject'.$subject;
+	$message = 'From: '.$name.' Email: '.$email.' Message: '.$message;
+	$headers = 'From: '. $email . "\r\n";
 
 // Contains the email parameters
 
-$success = mail($myAdd, $message, $headers);
+$success = mail($to, $message, $headers);
 
 // Sends the email if a success
 	if ($success) {
